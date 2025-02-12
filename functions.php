fe3da489b0bad77f80e4e1f07fbf516c1e18255f
Alexander Bigga <alexander@bigga.de>
2025-02-12 23:06:12 +0100

    Fix translation handling

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@
  * Load translation files from your child theme instead of the parent theme
  */
 function my_child_theme_locale() {
-    load_child_theme_textdomain( 'paddle', get_stylesheet_directory() . '/languages' );
+    load_theme_textdomain( 'paddle', get_stylesheet_directory() . '/languages' );
 }
-add_action( 'after_setup_theme', 'my_child_theme_locale' );
+add_action( 'after_setup_theme', 'my_child_theme_locale', 90 );
 
 /**
  * Register and enqueue childl theme styles
